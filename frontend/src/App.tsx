5c3f6ada284ab38ff3cdf10494fc7310b766c5de
HrudithL <hrudithl@gmail.com>
2024-10-20 20:43:20 -0500

    Added Logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,8 @@ import './App.css';
 import Icon from '@mdi/react';
 
 // Importing the images
-import profilePlaceholder from './assets/icons/Empty_Profile.png';
+import profilePlaceholder from './Empty_Profile.png';
+import logo from './GroupMeet.webp';
 
 import { mdiPlusBoxOutline } from '@mdi/js';
 import { mdiBellOutline } from '@mdi/js';
@@ -11,70 +12,70 @@ import { mdiMenu } from '@mdi/js';
 
 function App() {
     return (
-    <div className="app-container">
-        <div className="sidebar">
-            <h3 className="friend-header">Friends</h3>
+        <div className="app-container">
 
-        <div className="friend-list">
+            <div className="sidebar">
+                <img className="logo" src={logo} alt="Logo" />
+                <h3 className="friend-header">Friends</h3>
 
-          <label className="friend-item">
-            <img src={profilePlaceholder} alt="Friend #1" className="friend-avatar" />
-            <span>Friend #1</span>
-            <input type="checkbox" />
-          </label>
-          <label className="friend-item">
-            <img src={profilePlaceholder} alt="Friend #2" className="friend-avatar" />
-            <span>Friend #2</span>
-            <input type="checkbox" />
-          </label>
-          <label className="friend-item">
-            <img src={profilePlaceholder} alt="Friend #3" className="friend-avatar" />
-            <span>Friend #3</span>
-            <input type="checkbox" />
-          </label>
-          
-        </div>
+                <div className="friend-list">
 
-        <h3 className='group-header'>Groups</h3>
-            <div className="group-list">
-          <label className="group-item">
-            <img src={profilePlaceholder} alt="Group #1" className="group-avatar" />
-            <span>Group #1</span>
-            <input type="checkbox" />
-          </label>
-          <label className="group-item">
-            <img src={profilePlaceholder} alt="Group #2" className="group-avatar" />
-            <span>Group #2</span>
-            <input type="checkbox" />
-          </label>
-          <label className="group-item">
-            <img src={profilePlaceholder} alt="Group #3" className="group-avatar" />
-            <span>Group #3</span>
-            <input type="checkbox"/>
-          </label>
-        </div>
+                    <label className="friend-item">
+                        <img src={profilePlaceholder} alt="Friend #1" className="friend-avatar" />
+                        <span>Friend #1</span>
+                        <input type="checkbox" />
+                    </label>
+                    <label className="friend-item">
+                        <img src={profilePlaceholder} alt="Friend #2" className="friend-avatar" />
+                        <span>Friend #2</span>
+                        <input type="checkbox" />
+                    </label>
+                    <label className="friend-item">
+                        <img src={profilePlaceholder} alt="Friend #3" className="friend-avatar" />
+                        <span>Friend #3</span>
+                        <input type="checkbox" />
+                    </label>
+
+                </div>
+
+                <h3 className='group-header'>Groups</h3>
+                <div className="group-list">
+                    <label className="group-item">
+                        <img src={profilePlaceholder} alt="Group #1" className="group-avatar" />
+                        <span>Group #1</span>
+                        <input type="checkbox" />
+                    </label>
+                    <label className="group-item">
+                        <img src={profilePlaceholder} alt="Group #2" className="group-avatar" />
+                        <span>Group #2</span>
+                        <input type="checkbox" />
+                    </label>
+                    <label className="group-item">
+                        <img src={profilePlaceholder} alt="Group #3" className="group-avatar" />
+                        <span>Group #3</span>
+                        <input type="checkbox" />
+                    </label>
+                </div>
             </div>
 
             <div className="content">
                 <div className="top-buttons">
                     <button className='add-button'>
-                        <Icon className="add-icon" path={mdiPlusBoxOutline}/> 
+                        <Icon className="add-icon" path={mdiPlusBoxOutline} />
                     </button>
                     <button className='bell-button'>
-                    <Icon className="bell-icon" path={mdiBellOutline}/> 
+                        <Icon className="bell-icon" path={mdiBellOutline} />
                     </button>
                     <button className="invite-button">Send Invite</button>
                     <button className='menu-button'>
-                    <Icon className="menu-icon" path={mdiMenu}/> 
+                        <Icon className="menu-icon" path={mdiMenu} />
                     </button>
                 </div>
-
-                {/* Placeholder for the calendar component */}
                 <div className="calendar">
-                    <p>Your calendar will go here</p>
+                    <p>Calendar Goes Here</p>
                 </div>
             </div>
-    </div>
+        </div>
 
     );
 }
